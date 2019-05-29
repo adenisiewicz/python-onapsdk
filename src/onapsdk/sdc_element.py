db681d34e4b71e53f82fc62b634f923217fb4ee4
Sylvain Desbureaux <sylvain.desbureaux@orange.com>
2019-05-29 17:22:38 +0200

    :sparkles: create middle level class SdcElement
    Signed-off-by: Sylvain Desbureaux <sylvain.desbureaux@orange.com>

---
+++ src/onapsdk/sdc_element.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# SPDX-License-Identifier: Apache-2.0
+"""SDC Element module."""
+from onapsdk.onap_service import OnapService
+
+class SdcElement(OnapService):
+    """Mother Class of all SDC elements."""
+
+    def __init__(self):
+        """Initialize SDC element."""
+        super().__init__()
+        self.server: str = "SDC"
+        self.header: Dict[str, str] = self.__header()
+        self.base_front_url = "http://sdc.api.fe.simpledemo.onap.org:30206"
+        self.base_back_url = "http://sdc.api.be.simpledemo.onap.org:30205"
+
+    @staticmethod
+    def __header() -> Dict[str, str]:
+        """
+        Generate SDC header.
+
+        Returns:
+            The header dictionnary for SDC
+
+        """
+        return {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "USER_ID": "sdc_user_id",
+            "Authorization":
+                "Basic YWFpOktwOGJKNFNYc3pNMFdYbGhhazNlSGxjc2UyZ0F3ODR2YW9HR21KdlV5MlU=",
+            "cache-control": "no-cache"
+        }

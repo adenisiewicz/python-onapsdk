13f1632853c6fbbea627714efea4b42747353340
Sylvain Desbureaux <sylvain.desbureaux@orange.com>
2019-09-10 15:10:03 +0200

    :white_check_mark: continue testing services functions
    Signed-off-by: Sylvain Desbureaux <sylvain.desbureaux@orange.com>

--- tests/test_service.py
+++ tests/test_service.py
@@ -95,3 +95,80 @@ def test_distribution_id_setter():
     svc.identifier = "1234"
     svc.distribution_id = "4567"
     assert svc._distribution_id == "4567"
+
+@mock.patch.object(Service, '_create')
+def test_create(mock_create):
+    svc = Service()
+    svc.create()
+    mock_create.assert_called_once_with("service_create.json.j2", name="ONAP-test-Service")
+
+@mock.patch.object(Service, 'send_message')
+def test_add_resource_not_draft(mock_send):
+    svc = Service()
+    resource = SdcResource()
+    svc.add_resource(resource)
+    mock_send.assert_not_called()
+
+@mock.patch.object(Service, 'send_message')
+def test_add_resource_bad_result(mock_send):
+    svc = Service()
+    svc.unique_identifier = "45"
+    svc.status = const.DRAFT
+    mock_send.return_value = {}
+    resource = SdcResource()
+    resource.unique_identifier = "12"
+    resource.version = "40"
+    resource.name = "test"
+    assert svc.add_resource(resource) is None
+    mock_send.assert_called_once_with('POST', 'Add SdcResource to service', 'http://sdc.api.fe.simpledemo.onap.org:30206/sdc1/feProxy/rest/v1/catalog/services/45/resourceInstance', data='{\n  "name": "test",\n  "componentVersion": "40",\n  "posY": 100,\n  "posX": 200,\n  "uniqueId": "12",\n  "originType": "SDCRESOURCE",\n  "componentUid": "12",\n  "icon": "defaulticon"\n}')
+
+
+@mock.patch.object(Service, 'send_message')
+def test_add_resource_OK(mock_send):
+    svc = Service()
+    svc.unique_identifier = "45"
+    svc.status = const.DRAFT
+    mock_send.return_value = {'yes': 'indeed'}
+    resource = SdcResource()
+    resource.unique_identifier = "12"
+    resource.version = "40"
+    resource.name = "test"
+    result = svc.add_resource(resource)
+    assert result['yes'] == "indeed"
+    mock_send.assert_called_once_with('POST', 'Add SdcResource to service', 'http://sdc.api.fe.simpledemo.onap.org:30206/sdc1/feProxy/rest/v1/catalog/services/45/resourceInstance', data='{\n  "name": "test",\n  "componentVersion": "40",\n  "posY": 100,\n  "posX": 200,\n  "uniqueId": "12",\n  "originType": "SDCRESOURCE",\n  "componentUid": "12",\n  "icon": "defaulticon"\n}')
+
+@mock.patch.object(Service, '_verify_action_to_sdc')
+def test_checkin(mock_verify):
+    svc = Service()
+    svc.checkin()
+    mock_verify.assert_called_once_with(const.DRAFT, const.CHECKIN)
+
+@mock.patch.object(Service, '_verify_action_to_sdc')
+def test_submit(mock_verify):
+    svc = Service()
+    svc.submit()
+    mock_verify.assert_called_once_with(const.CHECKIN, const.SUBMIT_FOR_TESTING)
+
+@mock.patch.object(Service, '_verify_action_to_sdc')
+def test_start_certification(mock_verify):
+    svc = Service()
+    svc.start_certification()
+    mock_verify.assert_called_once_with(const.DRAFT, const.START_CERTIFICATION, headers={'Content-Type': 'application/json', 'Accept': 'application/json', 'USER_ID': 'jm0007', 'Authorization': 'Basic YWFpOktwOGJKNFNYc3pNMFdYbGhhazNlSGxjc2UyZ0F3ODR2YW9HR21KdlV5MlU=', 'X-ECOMP-InstanceID': 'onapsdk'})
+
+@mock.patch.object(Service, '_verify_action_to_sdc')
+def test_certify(mock_verify):
+    svc = Service()
+    svc.certify()
+    mock_verify.assert_called_once_with(const.DRAFT, const.CERTIFY, headers={'Content-Type': 'application/json', 'Accept': 'application/json', 'USER_ID': 'jm0007', 'Authorization': 'Basic YWFpOktwOGJKNFNYc3pNMFdYbGhhazNlSGxjc2UyZ0F3ODR2YW9HR21KdlV5MlU=', 'X-ECOMP-InstanceID': 'onapsdk'})
+
+@mock.patch.object(Service, '_verify_action_to_sdc')
+def test_approve(mock_verify):
+    svc = Service()
+    svc.approve()
+    mock_verify.assert_called_once_with(const.DRAFT, const.APPROVE, headers={'Content-Type': 'application/json', 'Accept': 'application/json', 'USER_ID': 'gv0001', 'Authorization': 'Basic YWFpOktwOGJKNFNYc3pNMFdYbGhhazNlSGxjc2UyZ0F3ODR2YW9HR21KdlV5MlU=', 'X-ECOMP-InstanceID': 'onapsdk'})
+
+@mock.patch.object(Service, '_verify_action_to_sdc')
+def test_distribute(mock_verify):
+    svc = Service()
+    svc.distribute()
+    mock_verify.assert_called_once_with(const.DRAFT, const.DISTRIBUTE, headers={'Content-Type': 'application/json', 'Accept': 'application/json', 'USER_ID': 'op0001', 'Authorization': 'Basic YWFpOktwOGJKNFNYc3pNMFdYbGhhazNlSGxjc2UyZ0F3ODR2YW9HR21KdlV5MlU=', 'X-ECOMP-InstanceID': 'onapsdk'})

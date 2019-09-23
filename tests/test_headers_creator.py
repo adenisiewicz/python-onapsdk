a26aa10ec28ea4adad98a8915a387fd2256c74df
umry8364 <morgan.richomme@orange.com>
2019-09-23 09:15:05 +0200

    WIP: work on service instantiation (VNF API)
    Signed-off-by: umry8364 <morgan.richomme@orange.com>

--- tests/test_headers_creator.py
+++ tests/test_headers_creator.py
@@ -1,11 +1,11 @@
 # SPDX-License-Identifier: Apache-2.0
 
-from onapsdk.utils.headers_creator import headers_aai_creator
-from onapsdk.utils.headers_creator import headers_sdc_creator
+
 from onapsdk.utils.headers_creator import headers_so_creator
 from onapsdk.utils.headers_creator import headers_sdc_tester
 from onapsdk.utils.headers_creator import headers_sdc_governor
 from onapsdk.utils.headers_creator import headers_sdc_operator
+from onapsdk.utils.headers_creator import headers_aai_creator
 
 
 def test_headers_sdc_creator():

260c896066454c34e96fedfb6f6ce617ae576824
mrichomme <morgan.richomme@orange.com>
2019-12-23 17:44:48 +0100

    xmas commits
    Signed-off-by: mrichomme <morgan.richomme@orange.com>

--- src/onapsdk/so_element.py
+++ src/onapsdk/so_element.py
@@ -33,53 +33,40 @@ class SoElement(OnapService):
         self._logger.info("attempting to create %s %s in SO",
                           type(self).__name__, self.name)
 
-        # url = self._base_create_url()
+        # Get subscribed ID
+        global_subscriber_id = self.get_subscriber_info()
+        self._logger.debug(
+            "Global subscriber retrieved: %s", global_subscriber_id)
+        # subscription_service_type
+        subscription_service_type = self.get_subscription_service_type(
+            kwargs['ns_name'])
+        self._logger.debug(
+            "Subscription service type found: %s", subscription_service_type)
 
-        # Generate VF model INFO
-        service_model = self.set_service_model_info(kwargs['ns_name'])
-        print(service_model)
-        vf_names = get_vf_list_from_tosca_file(kwargs['ns_model'])
-        vf_models_info = {}
-        for vf_name in vf_names:
-            vf_models_info[vf_name] = self.set_vf_model_info(vf_name)
-        print(vf_models_info)
+        # Get Service model
+        service_model = self.get_service_model_info(kwargs['ns_name'])
+
+        # get vf
+        vnf_instances = self.get_vnfs(**kwargs)
+
         # Generate Cloud INFO
-        cloud_info = self.set_cloud_info()
-        print(cloud_info)
+        cloud_info = self.get_cloud_info()
 
         # Create Service Instance payloadData
         if kwargs['ns_instantiation_mode'] == 'macro':
-            template = jinja_env().get_template("service_instance_macro.json.j2")
-            print(template)
+            template_macro = jinja_env().get_template(
+                "service_instance_macro.json.j2")
+            macro_payload = json.loads(template_macro.render(
+                global_subscriber_id=global_subscriber_id,
+                ns_instance_name=kwargs['ns_name'],
+                cloud_configuration=cloud_info,
+                subscription_service_type=subscription_service_type,
+                vnf_instances=vnf_instances,
+                service_model=service_model))
+            print(macro_payload)
 
     @classmethod
-    def set_service_model_info(cls, service_name):
-        """Retrieve Service Model info."""
-        service = Service(name=service_name)
-        template_service = jinja_env().get_template(
-            'service_instance_model_info.json.j2')
-        # Get service instance model
-        return json.loads(template_service.render(
-            model_invariant_id=service.unique_uuid,
-            model_name_version_id=service.identifier,
-            model_name=service.name,
-            model_version=service.version))
-
-    @classmethod
-    def set_vf_model_info(cls, vf_name):
-        """Retrieve the model info of the VFs."""
-        vf_object = Vf(name=vf_name)
-        template_service = jinja_env().get_template(
-            'vf_model_info.json.j2')
-        return json.loads(template_service.render(
-            vf_model_invariant_uuid=vf_object.unique_uuid,
-            vf_model_customization_id="????",
-            vf_model_version_id=vf_object.identifier,
-            vf_model_name=vf_object.name,
-            vf_model_version=vf_object.version))
-
-    @classmethod
-    def set_cloud_info(cls):
+    def get_cloud_info(cls):
         """Retrieve Cloud info."""
         # on pourrait imaginer de préciser le cloud ici en cas de Multicloud
         # en attendant on prendra le premier cloud venu..
@@ -87,10 +74,103 @@ class SoElement(OnapService):
         aai_info = aai.get_cloud_info()
         template_cloud = jinja_env().get_template(
             'cloud_configuration.json.j2')
-        return json.loads(template_cloud.render(
+        parsed = json.loads(template_cloud.render(
             cloud_region_id=aai_info['cloud_region_id'],
             tenant_id=aai_info['tenant_id'],
             cloud_owner=aai_info['cloud_owner']))
+        return json.dumps(parsed, indent=4)
+
+    @classmethod
+    def get_subscriber_info(cls):
+        """Get subscriber Info."""
+        aai = AaiElement()
+        aai_info = aai.get_customers()
+        return aai_info['customer'][0]['global-customer-id']
+
+    @classmethod
+    def get_subscription_service_type(cls, vf_name):
+        """Retrieve the model info of the VFs."""
+        vf_object = Vf(name=vf_name)
+        return vf_object.name
+
+    @classmethod
+    def get_service_model_info(cls, service_name):
+        """Retrieve Service Model info."""
+        service = Service(name=service_name)
+        template_service = jinja_env().get_template(
+            'service_instance_model_info.json.j2')
+        # Get service instance model
+        parsed = json.loads(template_service.render(
+            model_invariant_id=service.unique_uuid,
+            model_name_version_id=service.identifier,
+            model_name=service.name,
+            model_version=service.version))
+        return json.dumps(parsed, indent=4)
+
+    @classmethod
+    def get_vnf_model_info(cls, vf_name):
+        """Retrieve the model info of the VFs."""
+        vf_object = Vf(name=vf_name)
+        template_service = jinja_env().get_template(
+            'vnf_model_info.json.j2')
+        parsed = json.loads(template_service.render(
+            vnf_model_invariant_uuid=vf_object.unique_uuid,
+            vnf_model_customization_id="????",
+            vnf_model_version_id=vf_object.identifier,
+            vnf_model_name=vf_object.name,
+            vnf_model_version=vf_object.version,
+            vnf_model_instance_name=(vf_object.name + " 0")))
+        # we need also a vnf instance Name
+        # Usually it is found like that
+        # name: toto
+        # instance name: toto 0
+        # it can be retrieved from the tosca
+        return json.dumps(parsed, indent=4)
+
+    @classmethod
+    def get_vf_model_info(cls, vf_name):
+        """Retrieve the VF model info From Tosca?."""
+        template_service = jinja_env().get_template(
+            'vf_model_info.json.j2')
+        parsed = json.loads(template_service.render(
+            vf_model_invariant_uuid="pim",
+            vf_model_customization_id="pam",
+            vf_model_version_id="poum",
+            vf_model_name=vf_name,
+            vf_model_version="skjfhkj"))
+        return json.dumps(parsed, indent=4)
+
+    def get_vnfs(self, **kwargs):
+        """Get VNFs description for macro instantiation."""
+        vf_names = get_vf_list_from_tosca_file(kwargs['ns_model'])
+
+        for vf_name in vf_names:
+            template_vnf = jinja_env().get_template(
+                'vnf_instance_macro.json.j2')
+
+            self._logger.debug(kwargs['ns_name'])
+            self._logger.debug("----------------------> 1")
+            self._logger.debug(self.get_vnf_model_info(vf_name))
+            self._logger.debug("----------------------> 2")
+            self._logger.debug(self.get_cloud_info())
+            self._logger.debug("----------------------> 3")
+            self._logger.debug("vnf_%s", vf_name)
+            self._logger.debug("----------------------> 4")
+            self._logger.debug(self.get_vf_model_info(vf_name))
+            self._logger.debug("----------------------> 5")
+            self._logger.debug("vf_%s", vf_name)
+
+            vnfs_macro = json.loads(template_vnf.render(
+                vnf_model_info=self.get_vnf_model_info(vf_name),
+                cloud_configuration=self.get_cloud_info(),
+                vnf_instance_name="vnf_" + vf_name,
+                vnf_instance_param="",
+                vnf_model=self.get_vf_model_info(vf_name),
+                vnf_model_instance_name="vnf_" + kwargs['ns_name'],
+                vnf_instance_params=""))
+        self._logger.info("vnfs payload part built: %s", vnfs_macro)
+        return json.dumps(vnfs_macro, indent=4)
+
 
     @classmethod
     def _base_create_url(cls) -> str:

1fd27ff595342dd3136ea87a1e580d2109c2a124
Richomme <morgan.richomme@orange.com>
2020-02-04 18:33:19 +0000

    Update test_tosca_file_handler.py

--- tests/test_tosca_file_handler.py
+++ tests/test_tosca_file_handler.py
@@ -10,22 +10,15 @@
 # pylint: disable=missing-docstring
 
 import logging
-<<<<<<< HEAD
 import json
 import oyaml as yaml
-=======
->>>>>>> 31e66f4b201b6fed0d5c0ca5b7752d6d3ab992f3
 import os
 import os.path
 import unittest
 
 from onapsdk.utils.jinja import jinja_env
-<<<<<<< HEAD
 import onapsdk.utils.tosca_file_handler as tosca_file_handler
 
-=======
-import onapsdk.utils.tosca_file_handler as onap_test_utils
->>>>>>> 31e66f4b201b6fed0d5c0ca5b7752d6d3ab992f3
 
 __author__ = "Morgan Richomme <morgan.richomme@orange.com>"
 
@@ -37,18 +30,14 @@ class ToscaFileHandlerTestingBase(unittest.TestCase):
     logging.disable(logging.CRITICAL)
 
     _root_path = os.getcwd().rsplit('/onapsdk')[0]
-<<<<<<< HEAD
     _foo_path = _root_path +"/tests/data/service-Ubuntu16-template.yml"
-=======
-    _foo_path = "/src/onapsdk/templates/tosca_files/service-Foo-template.yml"
->>>>>>> 31e66f4b201b6fed0d5c0ca5b7752d6d3ab992f3
 
 
     def setUp(self):
         pass
 
     def test_get_parameter_from_yaml(self):
-<<<<<<< HEAD
+
         with open(self._foo_path) as f:
             model = json.dumps(yaml.safe_load(f))
         param = tosca_file_handler.get_parameter_from_yaml(
@@ -79,42 +68,6 @@ class ToscaFileHandlerTestingBase(unittest.TestCase):
         vf_list = tosca_file_handler.get_vf_list_from_tosca_file(model)
         self.assertEqual(vf_list[0], 'ubuntu16_VF')
 
-    # def get_vf_list_from_tosca_file_wrong_model(self):
-    #     with self.assertRaises(FileNotFoundError):
-    #         tosca_file_handler.get_vf_list_from_tosca_file(
-    #             self._root_path + "wrong_path")
-=======
-        param = onap_test_utils.get_parameter_from_yaml(
-            "metadata", self._root_path + self._foo_path)
-        self.assertEqual(param['name'], "vFW-service")
-
-    def test_get_wrong_parameter_from_yaml(self):
-        with self.assertRaises(ValueError):
-            onap_test_utils.get_parameter_from_yaml(
-                "wrong_parameter", self._root_path + self._foo_path)
-
-    def test_get_parameter_from_wrong_yaml(self):
-        with self.assertRaises(FileNotFoundError):
-            onap_test_utils.get_parameter_from_yaml(
-                "metadata", "wrong_path")
-
-    def test_get_random_string_generator(self):
-        self.assertEqual(
-            len(onap_test_utils.random_string_generator()),
-            6)
-
-    def test_get_model_from_tosca(self):
-        service_model = onap_test_utils.get_model_from_tosca(
-            self._root_path + self._foo_path)
-        self.assertEqual(
-            service_model['service_instance']['modelInfo']['modelType'],
-            "service")
-
-    def test_get_model_from_tosca_wrong_name(self):
-        with self.assertRaises(FileNotFoundError):
-            onap_test_utils.get_model_from_tosca(
-                self._root_path + "wrong_path")
->>>>>>> 31e66f4b201b6fed0d5c0ca5b7752d6d3ab992f3
 
 if __name__ == "__main__":
     # logging must be disabled else it calls time.time()

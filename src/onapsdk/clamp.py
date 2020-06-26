83b04e42f0f3e7557aec479ec1ce4474a94c4ecf
amine ezziati <aymane.ezziati@gmail.com>
2020-06-26 11:04:52 +0200

    fix doc errors
    Signed-off-by: amine ezziati <aymane.ezziati@gmail.com>

--- src/onapsdk/clamp.py
+++ src/onapsdk/clamp.py
@@ -60,16 +60,18 @@ class LoopInstance(Clamp):
         raise ValueError("Couldn't create the instance")
 
     def add_oprational_policy(self, policy_type: str, policy_version: str) -> bool:
-        """Add op policy to the loop instance"""
+        """Add op policy to the loop instance."""
         url = "{}/loop/addOperationaPolicy/{}/policyModel/{}/{}".\
               format(self.base_url, self.name, policy_type, policy_version)
         add_response = self.send_message_json('PUT', 'Create Operational Policy', url)
-        if add_response and (len(add_response["operationalPolicies"]) > len(self.details["operationalPolicies"])):
+        if (add_response and 
+           (len(add_response["operationalPolicies"]) > len(self.details["operationalPolicies"]))):
             self.details = add_response
             return True
         raise ValueError("Couldn't add the op policy")
 
     def update_microservice_policy(self) -> None:
+        """Add TCA config to microservice."""
         url = "{}/loop/updateMicroservicePolicy/{}".format(self.base_url, self.name)
         template = jinja_env().get_template("clamp_add_tca_config.json.j2")
         data = template.render(LOOP_name=self.name)
@@ -79,7 +81,7 @@ class LoopInstance(Clamp):
                                           data=data)
         if upload_result:
             self._logger.info("Files for TCA config %s have been uploaded to loop's microservice",
-                            self.name)
+                              self.name)
         else:
             self._logger.error("an error occured during file upload for TCA config to loop's microservice %s",
-                            self.name)
+                               self.name)

10922773b762b972b20d877c5799e0321ec95a15
Michal Jagiello <michal.jagiello@t-mobile.pl>
2020-07-03 08:03:51 +0000

    Preload information object

--- src/onapsdk/sdnc/preload.py
+++ src/onapsdk/sdnc/preload.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # SPDX-License-Identifier: Apache-2.0
 """SDNC preload module."""
-from typing import Dict, Iterable
+from typing import Any, Dict, Iterable
 
 from onapsdk.utils.headers_creator import headers_sdnc_creator
 from onapsdk.utils.jinja import jinja_env
@@ -10,10 +10,62 @@ from onapsdk.utils.jinja import jinja_env
 from .sdnc_element import SdncElement
 
 
-class NetworkPreload(SdncElement):
-    """Class to upload network module preload."""
+class Preload(SdncElement):
+    """Preload base class."""
 
     headers: Dict[str, str] = headers_sdnc_creator(SdncElement.headers)
+
+
+class PreloadInformation(Preload):
+    """Preload information."""
+
+    def __init__(self, preload_id: str, preload_type: str, preload_data: Dict[str, Any]) -> None:
+        """Preload information initialization.
+
+        Args:
+            preload_id (str): Preload id
+            preload_type (str): Preload type
+            preload_data (Dict[str, Any]): Preload data
+        """
+        super().__init__()
+        self.preload_id: str = preload_id
+        self.preload_type: str = preload_type
+        self.preload_data: Dict[str, Any] = preload_data
+
+    def __repr__(self) -> str:  # noqa
+        """Preload information human readble string.
+
+        Returns:
+            str: Preload information description
+
+        """
+        return (f"PreloadInformation(preload_id={self.preload_id}, "
+                f"preload_type={self.preload_type}, "
+                f"preload_data={self.preload_data})")
+
+    @classmethod
+    def get_all(cls) -> Iterable["PreloadInformation"]:
+        """Get all preload informations.
+
+        Get all uploaded preloads.
+
+        Yields:
+            PreloadInformation: Preload information object
+
+        """
+        for preload_information in \
+            cls.send_message_json(\
+                "GET",\
+                "Get SDNC preload information",\
+                f"{cls.base_url}/restconf/operational/GENERIC-RESOURCE-API:preload-information",\
+                exception=ValueError).get('preload-information', {}).get('preload-list', []):
+            yield PreloadInformation(preload_id=preload_information["preload-id"],
+                                     preload_type=preload_information["preload-type"],
+                                     preload_data=preload_information["preload-data"])
+
+
+class NetworkPreload(Preload):
+    """Class to upload network module preload."""
 
     @classmethod
     def upload_network_preload(cls,
@@ -48,10 +100,8 @@ class NetworkPreload(SdncElement):
         )
 
 
-class VfModulePreload(SdncElement):
+class VfModulePreload(Preload):
     """Class to upload vf module preload."""
-
-    headers: Dict[str, str] = headers_sdnc_creator(SdncElement.headers)
 
     @classmethod
     def upload_vf_module_preload(cls,  # pylint: disable=too-many-arguments
--- tests/test_preload.py
+++ tests/test_preload.py
@@ -1,8 +1,62 @@
+from collections.abc import Iterable
 from unittest import mock
+
 
 import pytest
 
-from onapsdk.sdnc.preload import VfModulePreload
+from onapsdk.sdnc.preload import PreloadInformation, VfModulePreload
+
+
+PRELOAD_INFORMATIONS = {
+    'preload-information': {
+        'preload-list': [
+            {
+                'preload-id': 'Python_ONAP_SDK_network_instance_338d5238-22fe-44d1-857a-223e2f6edd9b', 
+                'preload-type': 'network', 
+                'preload-data': {
+                    'preload-network-topology-information': {
+                        'physical-network-name': 'Not Aplicable', 
+                        'is-provider-network': False, 
+                        'is-external-network': False, 
+                        'network-topology-identifier-structure': {
+                            'network-technology': 'neutron', 
+                            'network-type': 'Generic NeutronNet', 
+                            'network-name': 'Python_ONAP_SDK_network_instance_338d5238-22fe-44d1-857a-223e2f6edd9b', 
+                            'network-role': 'integration_test_net'
+                        }, 
+                        'is-shared-network': False
+                    }, 
+                    'preload-oper-status': {
+                        'create-timestamp': '2020-06-26T09:12:03.708Z', 
+                        'order-status': 'PendingAssignment'
+                    }
+                }
+            }, 
+            {
+                'preload-id': 'Python_ONAP_SDK_network_instance_5d61bcf6-ec37-4cea-9d1b-744d0c2b75b9', 
+                'preload-type': 'network', 
+                'preload-data': {
+                    'preload-network-topology-information': {
+                        'is-provider-network': False, 
+                        'is-external-network': False, 
+                        'network-topology-identifier-structure': {
+                            'network-technology': 'neutron', 
+                            'network-type': 'Generic NeutronNet', 
+                            'network-name': 'Python_ONAP_SDK_network_instance_5d61bcf6-ec37-4cea-9d1b-744d0c2b75b9', 
+                            'network-id': '1234', 
+                            'network-role': 'integration_test_net'
+                        }, 
+                        'is-shared-network': False
+                    }, 
+                    'preload-oper-status': {
+                        'create-timestamp': '2020-06-25T12:22:35.939Z', 
+                        'order-status': 'PendingAssignment'
+                    }
+                }
+            }
+        ]
+    }
+}
 
 
 @mock.patch.object(VfModulePreload, "send_message_json")
@@ -16,3 +70,16 @@ def test_vf_module_preload_gr_api(mock_send_message_json):
     assert description == "Upload VF module preload using GENERIC-RESOURCE-API"
     assert url == (f"{VfModulePreload.base_url}/restconf/operations/"
                    "GENERIC-RESOURCE-API:preload-vf-module-topology-operation")
+
+
+@mock.patch.object(PreloadInformation, "send_message_json")
+def test_preload_information(mock_send_message_json):
+    mock_send_message_json.return_value = PRELOAD_INFORMATIONS
+    preload_informations = PreloadInformation.get_all()
+    assert isinstance(preload_informations, Iterable)
+    preload_informations_list = list(preload_informations)
+    assert len(preload_informations_list) == 2
+    preload_information = preload_informations_list[0]
+    assert isinstance(preload_information, PreloadInformation)
+    assert preload_information.preload_id == "Python_ONAP_SDK_network_instance_338d5238-22fe-44d1-857a-223e2f6edd9b"
+    assert preload_information.preload_type == "network"

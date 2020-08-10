82649a1a541d3a0851b6b3fb3c5e746728977b90
Eli Halych <illia.halych@t-mobile.pl>
2020-08-10 12:38:43 +0200

    Cleanup, cases grouping, links and paths in order
    Since integration testing is supposed to test the connectivity between
    different modules, the necessary parts were left. The code checks
    whether the API endpoints are available for a call, then it reads all
    necessary files for testing, then it calls objects for maing calls to
    the API, then it writes a new Blueprint to the file system ().

--- integration_tests/test_08_cds.py
+++ integration_tests/test_08_cds.py
@@ -1,67 +1,48 @@
+import os
 import pytest
 import requests
-import os
 
-from tempfile import TemporaryDirectory, TemporaryFile
+from tempfile import TemporaryDirectory
 
 from onapsdk.configuration import settings
-
-from onapsdk.cds.blueprint import Blueprint, CbaMetadata, Mapping, MappingSet, Workflow
-from onapsdk.cds.cds_element import CdsElement
-from onapsdk.cds.data_dictionary import DataDictionary, DataDictionarySet
-
+from onapsdk.cds.blueprint import Blueprint
+from onapsdk.cds.data_dictionary import DataDictionarySet
 
 
 @pytest.mark.integration
-def test_CDS_request():
-    """Integration tests for CDS. """
+def test_cds_connection():
 
-    # TODO
-    #  1. Read dd's from a file
-    #  2. Write dd's to the server
-    #  3. Read Blueprint from from a file
-    #  4. Enrich and publish
-    #  5. Write enriched blueprint to a file
+    TEST_DD_PATH = "./test_files/test_dd.json"
+    TEST_CBA_PATH = "./test_files/test_vLB_CBA_Python.zip"
 
-    # 0. define paths
-    # TODO move links or substitute with tempfiles
-    CdsElement._url = "http://127.0.0.1:8080"
-    tmp_path_dd = "C:\\Users\\IHalych\\Desktop\\Projects\\files\\dd.json"
-    tmp_path_cba = "C:\\Users\\IHalych\\Desktop\\Projects\\files\\vLB_CBA_Python.zip"
 
-    # 0. test URLs
-    response = requests.post("{}/api/v1/dictionary".format(CdsElement._url)) # TODO replace URL: settings.CDS_URL
-    json_response = response.json()
+    # Endpoint availability
+    response = requests.post("{}/api/v1/dictionary".format(settings.CDS_URL))
     assert response.status_code == 200
-    assert json_response['success'] == True
-    assert json_response is not None
 
-    response = requests.post("{}/api/v1/blueprint-model/enrich".format(CdsElement._url)) # TODO replace URL: settings.CDS_URL
+    response = requests.post("{}/api/v1/blueprint-model/enrich".format(settings.CDS_URL))
     assert response.status_code == 200
-    assert type(response.content) == bytes
 
-    response = requests.post("{}/api/v1/blueprint-model/publish".format(CdsElement._url)) # TODO replace URL: settings.CDS_URL
+    response = requests.post("{}/api/v1/blueprint-model/publish".format(settings.CDS_URL))
     assert response.status_code == 200
-    assert type(response.content) == bytes
 
 
-    #  1. Read dd's from a file
-    dd_set = DataDictionarySet.load_from_file(tmp_path_dd, False)
+    # Reads from the file system
+    dd_set = DataDictionarySet.load_from_file(TEST_DD_PATH, True)
+    blueprint = Blueprint.load_from_file(TEST_CBA_PATH)
 
-    #  2. Write dd's to the server
+
+    # Connection availability between CDS API and Blueprint/DataDictionarySet
     dd_set.upload()
-
-    #  3. Read Blueprint from  from a file
-    blueprint = Blueprint.load_from_file(tmp_path_cba)
     assert type(blueprint.cba_file_bytes) == bytes
 
-    #  4. Enrich and publish
     blueprint = blueprint.enrich()
     assert type(blueprint.cba_file_bytes) == bytes
-    
+
     blueprint.publish()
 
-    # #  5. Write enriched blueprint to a file
+
+    # Writes to the file system
     with TemporaryDirectory() as tmpdirname:
         path = os.path.join(tmpdirname, "test-CBA-enriched.zip")
         blueprint.save(path)
